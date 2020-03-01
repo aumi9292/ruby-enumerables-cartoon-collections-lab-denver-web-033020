ff0b63cf474323849bafe3d02a0e8624ebd74c2d
Austin Miller <austin.miller@colorado.edu>
2020-03-01 21:48:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(calls)
   calls.any? {|long| long.length > 4}
 end
 
-def find_the_cheese(cheeses)
-  cheeses.find {|item| cheeses.include? "cheddar" || "gouda" || "camembert"}
+def find_the_cheese(foods)
+  foods.find {|item| item == "cheddar" || "gouda" || "camembert"}
 #  cheese_types = ["cheddar", "gouda", "camembert"]
 end
